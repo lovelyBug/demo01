328c81c27c73b66b2f2cb99602f41d97b56e0541
clf <esx56487@qq.com>
2018-12-04 17:25:53 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img v-if="show" v-on:click="reverse" src="./assets/logo.png">
+    <router-view/>
+    <button v-on:click="reverse">Reverse</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      show: true
+    }
+  },
+  methods: {
+    reverse () {
+      this.show = !this.show
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li v-bind:key="todo.text" v-for="todo in todos">
+        {{ todo.text }}
+      </li>
+    </ul>
+    <p>{{ message }}</p>
+    <p>{{ reverseMessage }}</p>
+    <input v-model="message">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      todos: [
+        {text: 'JS'},
+        {text: 'CSS'},
+        {text: 'HTML'}
+      ],
+      message: 'Hello vue!'
+    }
+  },
+  computed: {
+    reverseMessage () {
+      return this.message.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
